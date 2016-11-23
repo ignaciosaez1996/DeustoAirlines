2070a0110e1fa1b28a230a6947ef8388dc774e7e
Ianire Andola <ianire.andola@opendeusto.es>
2016-11-23 16:19:05 +0100

    Gestores

---
+++ DeustoAirlines/src/LN/GestorCliente.java
@@ -0,0 +1,29 @@
+package LN;
+
+public class GestorCliente
+{
+	public void ClienteNuevo()
+	{
+		
+	}
+	public void ComprarBillete()
+	{
+		
+	}
+	public void CancelarBillete()
+	{
+		
+	}
+	public void ConsultarHorarios()
+	{
+		
+	}
+	public void HistorialCompras()
+	{
+		
+	}
+	public void JustificanteCompra()//Properties
+	{
+		
+	}
+}
---
+++ DeustoAirlines/src/LN/GestorTrabajador.java
@@ -0,0 +1,29 @@
+package LN;
+
+public class GestorTrabajador 
+{
+	public void CrearVuelos() //Ciudad de origen y destino distintas
+	{
+		
+	}
+	public void ConsultarVuelos()
+	{
+		
+	}
+	public void CancelarVuelos()
+	{
+		
+	}
+	public void AsignarAgendaDeTrabajo()//Calendario
+	{
+		
+	}
+	public void VerAgendaDeTrabajo()
+	{
+		
+	}
+	public void ConsultarIngresos()
+	{
+		
+	}
+}
