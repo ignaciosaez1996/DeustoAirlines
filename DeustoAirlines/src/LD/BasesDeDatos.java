c995254b2fccca7a94234d26cfb08060e8e1e160
MaitaneArtano <maitaneartano@opendeusto.es>
2016-11-23 11:23:40 +0100

    Avanzando con LN

--- DeustoAirlines/src/LD/BasesDeDatos.java
+++ DeustoAirlines/src/LD/BasesDeDatos.java
@@ -52,7 +52,7 @@ public class BasesDeDatos
 				JOptionPane.showMessageDialog(null, "El cierre de la conexión ha fallado");
 			}
 	}
-	
+
 	/** 
 	 * Devuelve la conexion si ha sido establecida previamente (#initBD()).
 	 * @return	Conexion con la BD, null si no se ha establecido correctamente.
